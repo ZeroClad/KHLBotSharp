55ed3ba1e60e7853f196990057f5054879b0dcf1
PoH98 <shunyuan98@gmail.com>
2021-07-14 15:32:47 +0800

    Forgot to push check-in

--- KHLBotSharp.NETCore3/Program.cs
+++ KHLBotSharp.NETCore3/Program.cs
@@ -11,28 +11,73 @@ namespace KHLBotSharp.NETCore3
     {
         static void Main(string[] args)
         {
-            if (!Directory.Exists("Profiles"))
+            if (args.Length > 1)
             {
-                Directory.CreateDirectory("Profiles");
-                Directory.CreateDirectory("Profiles\\DefaultBot");
-                Directory.CreateDirectory("Profiles\\DefaultBot\\Plugins");
-                File.Copy("defaultConfig.json", Path.Combine(Environment.CurrentDirectory, "Profiles\\DefaultBot\\config.json"));
-            }
-            var bots = Directory.GetDirectories("Profiles");
-            foreach (var bot in bots)
-            {
-                var config = JObject.Parse(File.ReadAllText(Path.Combine(bot, "config.json")));
-                var token = config["BotToken"].ToString();
-                if (string.IsNullOrEmpty(token))
+                var profileName = args[1];
+                if (args[0] == "-c")
                 {
-                    Console.WriteLine("Missing token for " + bot.Split("\\").Last() + ". Skipping startup");
-                    continue;
+                    if (!Directory.Exists("Profiles"))
+                    {
+                        Directory.CreateDirectory("Profiles");
+                    }
+                    if (!Directory.Exists("Profiles\\" + profileName))
+                    {
+                        Directory.CreateDirectory("Profiles\\" + profileName);
+                        Directory.CreateDirectory("Profiles\\" + profileName + "\\Plugins");
+                        File.Copy("defaultConfig.json", Path.Combine(Environment.CurrentDirectory, "Profiles\\" + profileName + "\\config.json"));
+                    }
+                    Console.WriteLine("Profile creation success!");
+                    return;
                 }
-                var botService = new BotService(token, bot);
-                _ = botService.Run();
+                else if (args[0] == "-r")
+                {
+                    if (!Directory.Exists("Profiles"))
+                    {
+                        Directory.CreateDirectory("Profiles");
+                    }
+                    if (!Directory.Exists("Profiles\\" + profileName))
+                    {
+                        Directory.CreateDirectory("Profiles\\" + profileName);
+                        Directory.CreateDirectory("Profiles\\" + profileName + "\\Plugins");
+                        File.Copy("defaultConfig.json", Path.Combine(Environment.CurrentDirectory, "Profiles\\" + profileName + "\\config.json"));
+                    }
+                    var bot = "Profiles\\" + profileName;
+                    var config = JObject.Parse(File.ReadAllText(Path.Combine(bot, "config.json")));
+                    var token = config["BotToken"].ToString();
+                    if (string.IsNullOrEmpty(token))
+                    {
+                        Console.WriteLine("Missing token for " + bot.Split("\\").Last() + ". Skipping startup");
+                    }
+                    var botService = new BotService(token, bot);
+                    _ = botService.Run();
+                }
             }
+            else
+            {
+                if (!Directory.Exists("Profiles"))
+                {
+                    Directory.CreateDirectory("Profiles");
+                    Directory.CreateDirectory("Profiles\\DefaultBot");
+                    Directory.CreateDirectory("Profiles\\DefaultBot\\Plugins");
+                    File.Copy("defaultConfig.json", Path.Combine(Environment.CurrentDirectory, "Profiles\\DefaultBot\\config.json"));
+                }
+                var bots = Directory.GetDirectories("Profiles");
+                foreach (var bot in bots)
+                {
+                    var config = JObject.Parse(File.ReadAllText(Path.Combine(bot, "config.json")));
+                    var token = config["BotToken"].ToString();
+                    if (string.IsNullOrEmpty(token))
+                    {
+                        Console.WriteLine("Missing token for " + bot.Split("\\").Last() + ". Skipping startup");
+                        continue;
+                    }
+                    var botService = new BotService(token, bot);
+                    _ = botService.Run();
+                }
+            }
+
             Console.WriteLine("All bots are loaded");
-            Thread.Sleep(Timeout.Infinite);
+            Thread.Sleep(-1);
         }
     }
 }
