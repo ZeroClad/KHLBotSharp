29f6e7c7547cf7a6f8cf621e26c0f0c1cffadfce
PoH98 <shunyuan98@gmail.com>
2021-11-19 23:27:59 +0800

    Added NET6 Webhook

---
+++ KHLBotSharp.WebHook.NET6/Program.cs
@@ -0,0 +1,24 @@
+using KHLBotSharp.Core.BotHost;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace KHLBotSharp.WebHook.NET6
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Welcome.Print();
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                    .ConfigureWebHostDefaults(webBuilder =>
+                    {
+                        webBuilder.UseStartup<Startup>();
+                    });
+        }
+    }
+}
