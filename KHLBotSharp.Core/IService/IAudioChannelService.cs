0c2f010884a330d484be76332a201a04f4100bb9
PoH98 <shunyuan98@gmail.com>
2022-01-02 17:49:37 +0800

    添加了khl-voice和相关Service

---
+++ KHLBotSharp.Core/IService/IAudioChannelService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KHLBotSharp.Core.IService
+{
+    public interface IAudioChannelService
+    {
+        /// <summary>
+        /// 解压开黑啦khl-voice，如果已经存在则自动跳过啥都不干，推荐在插件加载阶段直接运行
+        /// </summary>
+        /// <returns></returns>
+        Task Init();
+        /// <summary>
+        /// 播放文件或者链接上的音频, 你也可以使用rtmp链接。注意Play功能会直接强制切换当前正在播放，因此如果有需要自己搞个列队还请自己搞
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        Task Play(string fileName, string channelId, bool repeat = false);
+        /// <summary>
+        /// 停止所有播放
+        /// </summary>
+        /// <returns></returns>
+        Task Stop();
+    }
+}
---
+++ KHLBotSharp.Core/Services/AudioChannelService.cs
@@ -0,0 +1,108 @@
+﻿using KHLBotSharp.Core.IService;
+using KHLBotSharp.Core.Models.Config;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace KHLBotSharp.Core.Services
+{
+    public class AudioChannelService : IAudioChannelService
+    {
+        private readonly string token;
+        private Process player;
+        public AudioChannelService(IBotConfigSettings config)
+        {
+            token = config.BotToken;
+        }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public Task Init()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                Process p = new Process()
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = "cmd.exe",
+                        Arguments = "/c ffmpeg -v",
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        CreateNoWindow = true,
+                        WindowStyle = ProcessWindowStyle.Hidden,
+                        WorkingDirectory = Environment.CurrentDirectory
+                    }
+                };
+                p.Start();
+                string ffmpegcheck = p.StandardOutput.ReadToEnd();
+                if (ffmpegcheck.Contains("not recognized"))
+                {
+                    //no install ffmpeg
+                    throw new FileNotFoundException("ffmpeg not found! Please download it at https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-full.7z and setup environment path!");
+                }
+                if (!File.Exists("khl-voice.exe"))
+                {
+                    var zipArchive = new ZipArchive(new MemoryStream(Resource.khl_voice_exe));
+                    zipArchive.ExtractToDirectory(Environment.CurrentDirectory);
+                }
+            }
+            else
+                if (!File.Exists("khl-voice"))
+                {
+                var zipArchive = new ZipArchive(new MemoryStream(Resource.khl_voice));
+                zipArchive.ExtractToDirectory(Environment.CurrentDirectory);
+            }
+            return Task.CompletedTask;
+        }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public Task Play(string fileName, string channelId, bool repeat  = false)
+        {
+            return Task.Run(() =>
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    player = new Process()
+                    {
+                        StartInfo = new ProcessStartInfo
+                        {
+                            FileName = "khl-voice.exe",
+                            Arguments = "-i " + fileName + " -t " + token + " -c " + channelId,
+                            CreateNoWindow = true,
+                            WindowStyle = ProcessWindowStyle.Hidden,
+                            WorkingDirectory = Environment.CurrentDirectory
+                        }
+                    };
+                }
+                else
+                {
+                    player = new Process()
+                    {
+                        StartInfo = new ProcessStartInfo
+                        {
+                            FileName = "khl-voice",
+                            Arguments = "-i " + fileName + " -t " + token + " -c " + channelId,
+                            CreateNoWindow = true,
+                            WindowStyle = ProcessWindowStyle.Hidden,
+                            WorkingDirectory = Environment.CurrentDirectory
+                        }
+                    };
+                }
+                player.Start();
+            });
+        }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public Task Stop()
+        {
+            player.Kill();
+            return Task.CompletedTask;
+        }
+    }
+}
